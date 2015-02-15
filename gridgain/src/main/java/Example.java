7e89b960e8382428fc8c3a8c6071f93f078125b8
Pavel Blinov <blinov.pavel@gmail.com>
2015-02-16 01:30:04 +0300

    GridGain

---
+++ gridgain/src/main/java/Example.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+/**
+ * Created by Pavel on 16.02.2015.
+ */
+public class Example {
+    private static final Logger LOGGER = Logger.getLogger(Example.class.toString());
+
+    public static void main(String [] args) {
+        LOGGER.info("Starting...");
+        LOGGER.info("Stop");
+    }
+}
