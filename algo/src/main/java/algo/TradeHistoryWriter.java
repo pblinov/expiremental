37fa698c940072216dd7da6a39f1a4beca30b43e
Pavel Blinov <blinov.pavel@gmail.com>
2019-01-13 01:41:25 +0300

    Collect balances and trades in CSV files

--- algo/src/main/java/algo/TradeHistoryWriter.java
+++ algo/src/main/java/algo/TradeHistoryWriter.java
@@ -9,10 +9,13 @@ import org.slf4j.LoggerFactory;
 import java.io.Closeable;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class TradeHistoryWriter implements Closeable {
     private static final Logger LOGGER = LoggerFactory.getLogger(TradeHistoryWriter.class);
 
+    private final DateFormat format = new SimpleDateFormat("YYYY-MM-dd");
     private final CSVPrinter printer;
 
     public TradeHistoryWriter() throws IOException {
@@ -46,10 +49,10 @@ public class TradeHistoryWriter implements Closeable {
                     trade.getId(),
                     trade.getFeeAmount(),
                     trade.getFeeCurrency(),
-                    trade.getTimestamp()
+                    format.format(trade.getTimestamp())
             );
         } catch (IOException e) {
-            LOGGER.warn("Cannot write trade", e);
+            LOGGER.warn("{} Cannot write trade: {}", exchange, trade, e);
         }
     }
 }
